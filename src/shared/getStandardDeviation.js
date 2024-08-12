b4619c7cb4cc23a35a5f4b21147805536a6b81c3
OzgeYurtsever <ozge.ikiz.yurtsever@gmail.com>
2024-08-12 16:21:48 -0700

    Add more consoles and build

--- src/shared/getStandardDeviation.js
+++ src/shared/getStandardDeviation.js
@@ -7,8 +7,13 @@
 
 function getStandardDeviation(storedPixelData) {
   const numPixels = storedPixelData.length;
+  console.log(' ---> numPixels', numPixels);
+  const mean1 = storedPixelData.reduce((acc, val) => acc + val, 0);
+  console.log(' ---> mean 1', mean1);
   const mean = storedPixelData.reduce((acc, val) => acc + val, 0) / numPixels;
   const arr = storedPixelData.map((x) => Math.pow(x - mean, 2));
+  console.log(' ----> arr', arr);
+  console.log(' ====> stdev 1', Math.sqrt(arr.reduce((a, b) => a + b)));
   const standardDeviation = Math.sqrt(arr.reduce((a, b) => a + b) / numPixels);
   return standardDeviation;
 }
