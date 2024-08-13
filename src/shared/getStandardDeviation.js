38664b67c13f380469946825b890b1be831f40fb
OzgeYurtsever <ozge.ikiz.yurtsever@gmail.com>
2024-08-13 09:17:08 -0700

    Hide comments

--- src/shared/getStandardDeviation.js
+++ src/shared/getStandardDeviation.js
@@ -8,18 +8,18 @@
 function getStandardDeviation(storedPixelData) {
   const arr32 = Int32Array.from(storedPixelData);
   const numPixels = arr32.length;
-  console.log(' ---> numPixels', numPixels);
+  // console.log(' ---> numPixels', numPixels);
   const mean1 = arr32.reduce((acc, val) => acc + val, 0);
-  console.log(' ---> mean 1', mean1);
+  // console.log(' ---> mean 1', mean1);
   const mean = arr32.reduce((acc, val) => acc + val, 0) / numPixels;
   const arr = arr32.map((x) => Math.pow(x - mean, 2));
-  console.log(' ----> arr', arr);
-  console.log(' ====> stdev 1', Math.sqrt(arr.reduce((a, b) => a + b)));
+  // console.log(' ----> arr', arr);
+  // console.log(' ====> stdev 1', Math.sqrt(arr.reduce((a, b) => a + b)));
   const standardDeviation = Math.sqrt(arr.reduce((a, b) => a + b) / numPixels);
   arr32.slice(0, 5).forEach((x) => {
-    console.log(' ---> x', x);
-    console.log(' ---> mean', mean);
-    console.log(' ---> result', Math.pow(x - mean, 2));
+    // console.log(' ---> x', x);
+    // console.log(' ---> mean', mean);
+    // console.log(' ---> result', Math.pow(x - mean, 2));
   });
   return standardDeviation;
 }
